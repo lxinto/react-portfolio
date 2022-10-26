0855587e118f8ea3208c682e6ce58a49504085ca
lxinto <khintibidzel@gmail.com>
2022-10-26 18:27:33 +0400

    norefferer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -16,19 +16,19 @@ const Contact = () => {
               <MdAlternateEmail size="25px"/>
               <h4>Email</h4>
               <h5>khintibidzel@gmail.com</h5>
-              <a href="mailto:khintibidzel@gmail.com" target="_blank">Send me an Email</a>
+              <a href="mailto:khintibidzel@gmail.com" target="_blank" rel="noreferrer">Send me an Email</a>
             </article>
             <article className="contact__option">
               <RiMessengerLine size="25px"/>
               <h4>Messenger</h4>
               <h5>Luka Khintibidze</h5>
-              <a href="https://m.me/lkhintibidze" target="_blank">Send me a message</a>
+              <a href="https://m.me/lkhintibidze" target="_blank" rel="noreferrer">Send me a message</a>
             </article>
             <article className="contact__option">
               <BsLinkedin size="25px"/>
               <h4>LinkedIn</h4>
               <h5>@khintibidzel</h5>
-              <a href="https://www.linkedin.com/in/khintibidzel/" target="_blank">Lets connect!</a>
+              <a href="https://www.linkedin.com/in/khintibidzel/" target="_blank" rel="noreferrer">Lets connect!</a>
             </article>
           </div>
 
